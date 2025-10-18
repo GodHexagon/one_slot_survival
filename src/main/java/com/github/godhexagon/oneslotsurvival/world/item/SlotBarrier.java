79c0444e40e4f7668b791ec4247632bbe93a08fe
GodHexagon <god.hexagon.bifry@gmail.com>
2025-10-18 21:08:53 +0900

    スロットバリアアイテムの不要なオーバーライドを削除した

--- src/main/java/com/github/godhexagon/oneslotsurvival/world/item/SlotBarrier.java
+++ src/main/java/com/github/godhexagon/oneslotsurvival/world/item/SlotBarrier.java
@@ -1,13 +1,8 @@
 package com.github.godhexagon.oneslotsurvival.world.item;
 
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.item.ItemEntity;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * インベントリスロットをブロックするカスタムバリアアイテム
@@ -17,12 +12,6 @@ public class SlotBarrier extends Item {
 
     public SlotBarrier(Properties properties) {
         super(properties);
-    }
-
-    @Override
-    public @NotNull InteractionResult use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand hand) {
-        // プレイヤーがこのアイテムを使用するのを防ぐ
-        return InteractionResult.FAIL;
     }
 
     @Override
